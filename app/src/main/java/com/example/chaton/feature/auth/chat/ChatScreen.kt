8e09eb32b502b1f67a7d4a96fbd57900223c923f
Rabbi <rabbyislam7777@gmail.com>
2024-11-17 23:38:34 +0600

    ChatViewModel complete

--- app/src/main/java/com/example/chaton/feature/auth/chat/ChatScreen.kt
+++ app/src/main/java/com/example/chaton/feature/auth/chat/ChatScreen.kt
@@ -1,2 +1,46 @@
 package com.example.chaton.feature.auth.chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.chaton.model.Message
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+
+@Composable
+fun ChatBubble(message: Message) {
+    val isCurrentUser = message.senderId == Firebase.auth.currentUser?.uid
+    val bubbleColor = if (isCurrentUser) {
+        Color.Blue
+    } else {
+        Color.Green
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+
+    ) {
+        val alignment = if (!isCurrentUser) Alignment.CenterStart else Alignment.CenterEnd
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .background(color = bubbleColor, shape = RoundedCornerShape(8.dp))
+                .align(alignment)
+        ) {
+            Text(
+                text = message.message, color = Color.White, modifier = Modifier.padding(8.dp)
+            )
+        }
+
+    }
+}
--- app/src/main/java/com/example/chaton/feature/auth/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/chaton/feature/auth/chat/ChatViewModel.kt
@@ -1,13 +1,61 @@
 package com.example.chaton.feature.auth.chat
 
 import androidx.lifecycle.ViewModel
+import com.example.chaton.model.Message
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
-class ChatViewModel @Inject constructor():ViewModel(){
-    private val firebaseDatabase = Firebase.database
+class ChatViewModel @Inject constructor() : ViewModel() {
+
+    private val _message = MutableStateFlow<List<Message>>(emptyList())
+    val message = _message.asStateFlow()
+    private val db = Firebase.database
+
+
+    fun sendMessage(channelId: String, messageText: String){
+        val message = Message(
+            id = db.reference.push().key?: UUID.randomUUID().toString(),
+            senderId = Firebase.auth.currentUser?.uid?: "",
+            message = messageText,
+            createdAt = System.currentTimeMillis(),
+            senderName = Firebase.auth.currentUser?.displayName?: "" ,
+            senderImage = null,
+            imageUrl = null
+        )
+
+        val key = db.getReference("message").child(channelId).push().setValue(message)
+
+    }
+
+    fun listenForMessage(channelId: String) {
+        db.getReference("message").child(channelId).child("message").orderByChild("createdAt")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val list = mutableListOf<Message>()
+                    snapshot.children.forEach { data->
+                        val message = data.getValue(Message::class.java)
+                        message?.let {
+                            list.add(message)
+                        }
+                    }
+                    _message.value = list
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+    }
 
 }
